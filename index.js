4f551717cad0b95a8c2aaaa4671ccba707dc7498
Artem <artemlyksa@gmail.com>
2019-10-16 09:46:46 +0300

    Inserted rentals middleware. Added fawn package to simulate db transaction

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 const app = express();
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.Port || 3000;
 app.listen(port, () => {
--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,12 @@
 const { Rental, validate } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const { Customer } = require('../models/customer');
+const Fawn = require('fawn');
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
     const rentals = await Rental.find().sort('-dateOut');
@@ -16,12 +20,12 @@ router.post('/', async (req, res) => {
         return res.status(400).send(error.details[0].message);
     }
 
-    const customer = await Customer.findById(req.body.customerId); 
+    const customer = await Customer.findById(req.body.customerId);
     if (!customer) {
         return res.status(404).send(`The customer with given id = ${req.body.customerId} not found`);
     }
 
-    const movie = await Customer.findById(req.body.movieId); 
+    const movie = await Movie.findById(req.body.movieId);
     if (!movie) {
         return res.status(404).send(`The movie with given id = ${req.body.movieId} not found`);
     }
@@ -42,9 +46,22 @@ router.post('/', async (req, res) => {
             title: movie.title,
             dailyRentalRate: movie.dailyRentalRate
         },
-        rentalFee: 
-    })
+    });
 
-    rental = await rental.save();
-    res.send(rental);
+    try {
+        new Fawn.Task()
+            .save('rentals', rental)
+            .update('movies', { _id: movie._id }, {
+                $inc: { numberInStock: -1 }
+            })
+            .run();
+        res.send(rental);
+    }
+    catch (ex) {
+        return res.status(500).send('Something failed');
+    }
+
+
 });
+
+module.exports = router;
