e50df1f8185bfe12c106a33f7581d3d82ccd4409
Artem <artemlyksa@gmail.com>
2019-10-18 13:37:36 +0300

    Added passing of jwt via response header

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 router.post('/', async (req, res) => {
 
@@ -10,13 +12,15 @@ router.post('/', async (req, res) => {
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
-
+2
     const user = new User(_.pick(req.body, ['name', 'email', 'password']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
 
     try {
         await user.save();
+        const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+        res.header('x-auth-token', token);
         res.send(_.pick(user, ['_id', 'name', 'email']));
     }
     catch (ex) {
