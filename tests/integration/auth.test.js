42689f020b4b42a5ff87902b2b37d4c7f886bd23
Artem <artemlyksa@gmail.com>
2019-11-04 13:06:48 +0200

    Added integration test for auth middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,43 @@
+const { User } = require('../../models/user');
+const { Genre } = require('../../models/genre');
+const request = require('supertest');
+let server;
+
+describe('auth middleware', () => {
+    beforeEach(() => { server = require('../../index'); });
+    afterEach(async () => { 
+        await Genre.remove({});
+        server.close(); 
+    });
+
+    let token;
+
+    beforeEach(() => {
+        token = new User().generateAuthToken();
+    });
+
+    const execute = function () {
+        return request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({ name: 'genre1' });
+    };
+
+    it('should return 401 if token is not provided', async () => {
+        token = '';
+        const res = await execute();
+        expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if token is not valid', async () => {
+        token = 'a';
+        const res = await execute();
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 200 if token is valid', async () => {
+        const res = await execute();
+        expect(res.status).toBe(200);
+    });
+
+});
