1dd18ebf88368b73f1d2cc278654d10eb1b22f74
Artem <artemlyksa@gmail.com>
2019-11-14 16:37:02 +0200

    Moved calculation logic into instance method

--- routes/returns.js
+++ routes/returns.js
@@ -5,25 +5,19 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const validate = require('../middleware/validate');
 const router = express.Router();
-const moment = require('moment');
 
 router.post('/', [auth, validate(validateReturn)], async (req, res) => {
 
     const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
     if (!rental) res.status(404).send('rental not found');
-
     if (rental.dateReturned) res.status(400).send('rental already processed');
 
-    rental.dateReturned = new Date();
-    const rentalDays = moment().diff(rental.dateOut, 'days');
-    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
-    await rental.save();
-
+    await rental.return();
     await Movie.updateOne({ _id: rental.movie._id }, {
         $inc: { numberInStock: 1 }
     });
 
-    res.status(200).send(rental);
+    res.send(rental);
 });
 
 function validateReturn(req) {
